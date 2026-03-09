a51b043086b72a851174a5154a2aef1f47b2061e
James Ring <jring@sva.edu>
2026-03-09 18:17:13 -0400

    Add files via upload

---
+++ fbx_processer.py
@@ -0,0 +1,26 @@
+import hou
+import argparse
+import os
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+parser = argparse.ArgumentParser(description='Cook FBX files and render to MP4 using Houdini.')
+parser.add_argument('--hip', default = os.path.join(dir_path, 'houdini_files', 'autosplatscene_v01.hipnc'), type=str, help='Path to the Houdini .hip file')
+parser.add_argument('--files', type=str, help='Path to the directory containing the FBX files')
+parser.add_argument('--render', default = '$HIP/renders', type=str, help='Path to the directory where the rendered MP4 files will be saved')
+
+args = parser.parse_args()
+hip_file = args.hip
+fbx_files = args.files
+render_path = args.render
+
+hou.hipFile.load(hip_file)
+
+filetop = hou.node('/obj/topnet1/filepattern1')
+filetop.parm('pattern').set(fbx_files + '/*.fbx')
+
+encodetop = hou.node('/obj/topnet1/ffmpegencodevideo1')
+encodetop.parm('outputfilepath').set(render_path + '/$HIPNAME.$OS.`@pdg_index`.mp4')
+
+encodetop.dirtyAllWorkItems(True)
+encodetop.cookWorkItems(block=True)
