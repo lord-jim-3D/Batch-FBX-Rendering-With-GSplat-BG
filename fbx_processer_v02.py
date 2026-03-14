9f906304c50fe3f7a594bff61eb34e0e2e657b22
James Ring <jring@sva.edu>
2026-03-14 19:06:56 -0400

    Added cancel function

--- fbx_processer_v02.py
+++ fbx_processer_v02.py
@@ -1,9 +1,20 @@
 import hou
 import argparse
 import os
+import signal
+import sys
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
+def signal_handler(sig, frame):
+    print('\nCancellation requested. Stopping work items...')
+    try:
+        encodetop.cancelWorkItems()
+    except:
+        pass
+    sys.exit(0)
+
+# Get required
 parser = argparse.ArgumentParser(description='Cook FBX files and render to MP4 using Houdini.')
 parser.add_argument('--hip', default = os.path.join(dir_path, 'houdini_files', 'autosplatscene_v02.hipnc'), type=str, help='Path to the Houdini .hip file')
 parser.add_argument('--files', type=str, help='Path to the directory containing the FBX files')
@@ -16,8 +27,11 @@ fbx_files = args.files
 render_path = args.render
 splat_file = args.splat
 
+
+# Run the houdini file
 hou.hipFile.load(hip_file)
 
+#Insert the paths into the topnet
 splattop = hou.node('/obj/topnet1/splat_files')
 splattop.parm('pattern').set(splat_file + '/*.ply')
 
@@ -27,5 +41,14 @@ filetop.parm('pattern').set(fbx_files + '/*.fbx')
 encodetop = hou.node('/obj/topnet1/ffmpegencodevideo1')
 encodetop.parm('outputfilepath').set(render_path + '/' + '`@splatname`_`@filename`.mp4')
 
+# Register signal handler for Ctrl+C
+signal.signal(signal.SIGINT, signal_handler)
+
+print("Press Ctrl+C to cancel the process.")
+print("Topnet is running.")
+
+#Run the top net
 encodetop.dirtyAllWorkItems(True)
 encodetop.cookWorkItems(block=True)
+
+print("Everything has been rendered!")
